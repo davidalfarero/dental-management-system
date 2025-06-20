53aaf8ea967d19cbc2bf7b1b5571e74b2660e827
codedave <208051940+codedave15@users.noreply.github.com>
2025-06-21 04:28:20 +0800

    added dark mode toggle button

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from './UI';
-import { Menu, X } from 'lucide-react';
+import { Menu, Moon, Sun, X } from 'lucide-react';
 
 const Navbar = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [scrolled, setScrolled] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   const navlinks = [
     { name: 'About', href: '#about' },
@@ -13,9 +15,24 @@ const Navbar = () => {
     { name: 'Contact Us', href: '#contact' }
   ];
 
+  useEffect(() => {
+    const handleScroll = () => {
+      setScrolled(window.scrollY > 10);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
   return (
-    <nav className="fixed top-0 w-full h-15 md:h-20 flex items-center border-b border-base-300 z-50
-    bg-base-100">
+    <nav className={`fixed top-0 w-full h-15 md:h-20 flex items-center border-b border-base-300 z-50
+      ${scrolled
+        ? 'bg-base-100/60 backdrop-blur-md border-b border-base-300 shadow-sm'
+        : 'bg-base-100'}
+      }
+      `}
+
+    >
       <div className='max-w-5xl mx-auto w-full flex items-center justify-between gap-4 px-4'>
 
         <div className='flex-1 md:flex-none'>
@@ -40,6 +57,13 @@ const Navbar = () => {
               {name}
             </a>
           ))}
+        </div>
+
+        <div
+          onClick={() => setDarkMode((prev) => !prev)}
+          className='cursor-pointer p-2 rounded-xl hover:bg-base-300 transition-colors'
+        >
+          {darkMode ? <Sun className='text-yellow-400' /> : <Moon />}
         </div>
 
         {/* Todo: Book online function */}
